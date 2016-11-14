1996b40f6f47ede16b1405b76eec779a7dd72aff
Tu Hoang <hatu87@gmail.com>
2016-11-14 14:14:34 +0700

    create, edit, update blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,41 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def create
+    @article = Article.new(article_params)
+    byebug
+    unless(@article.save)
+      flash[:error] = "Error: Cannot create new article"
+      render 'new'
+    else
+      redirect_to articles_path
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+
+    if (article.update(article_params).present?)
+      redirect_to article_path(params[:id])
+    else
+      render 'edit'
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get '/articles', to: 'articles#index'
+  resources :articles, only: [:index, :new, :create, :show, :edit, :update]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hello', to: 'hello#index'
   post '/hello', to: 'hello#say_hello'
 end
