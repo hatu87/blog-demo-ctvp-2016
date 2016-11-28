acd7af7f7d6dc9cbff547a841ea1cf70e7761e15
Tu Hoang <hatu87@gmail.com>
2016-11-28 14:18:13 +0700

    add routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :new, :create, :show, :edit, :update] do
     resources :comments, only: [:create]
   end
+  # get '/articles/:article_id/comments', to: 'comments#index'
 
+  # get '/articles', to: 'articles#index'
+  # post '/articles', to: 'articles#create'
   get '/hello', to: 'hello#index'
   post '/hello', to: 'hello#say_hello'
 end
