4177ed57bf35430f0ad120673a88ca8681f39103
Tu Hoang <hatu87@gmail.com>
2016-12-05 14:48:11 +0700

    Add jquery ajax

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -11,5 +11,64 @@ $(document).ready(function () {
     $(this).addClass('hover-row')
   }, function() {
     $(this).removeClass('hover-row')
-  })
+  });
+
+  loadArticles();
 });
+
+function loadArticles() {
+  var table = document.getElementById('jsarticles');
+  table.appendChild(createLoadingElement());
+
+  setTimeout(function() {
+    $.get({
+      url: 'http://localhost:3000/articles.json',
+      complete: function(result) {
+        clearElement('jsarticles');
+
+        var articles = $.parseJSON(result.responseText);
+
+        // create header
+        var container = createContainer('table');
+
+        articles.forEach(function(value) {
+
+          var element = document.createElement('tr');
+          var col = document.createElement('td');
+          col.innerText = value.id;
+          element.appendChild(col);
+
+          col = document.createElement('td');
+          col.innerText = value.title;
+          element.appendChild(col);
+
+          container.appendChild(element);
+        });
+
+        $('#jsarticles')
+          .append(container);
+      }
+    });
+  }, 2000);
+}
+
+function createContainer(tagName) {
+  var container = document.createElement(tagName);
+  $(container).addClass('table');
+  $(container).html("<tr><th>#</th><th>title</th></tr>");
+
+  return container;
+}
+
+function clearElement(id) {
+  var element = document.getElementById(id);
+  element.innerText = "";
+}
+
+function createLoadingElement() {
+  var element = document.createElement('div');
+
+  element.innerText = "Loading...";
+
+  return element;
+}
